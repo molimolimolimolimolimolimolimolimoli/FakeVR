2f531f55a9eb84c21bf36b0c2a78cb054806aad0
moli <133854689+molimolimolimolimolimolimolimolimoli@users.noreply.github.com>
2026-06-27 12:22:10 +0300

    Update build.mjs

--- build.mjs
+++ build.mjs
@@ -1,4 +1,4 @@
-import { readFile, writeFile, mkdir } from "fs/promises";
+import { readFile, writeFile, readdir } from "fs/promises";
 import { extname } from "path";
 import { createHash } from "crypto";
 
@@ -17,7 +17,7 @@ const plugins = [
     commonjs(),
     url({
         include: ["**/*.svg", "**/*.png", "**/*.jpg", "**/*.gif"],
-        limit: 0,
+        limit: 0, // inline files below this size (0 = never inline)
     }),
     {
         name: "swc",
@@ -53,37 +53,41 @@ const plugins = [
     esbuild({ minify: true }),
 ];
 
-const manifest = JSON.parse(await readFile("./manifest.json"));
-const outPath = "./dist/index.js";
+for (let plug of await readdir("./plugins")) {
+    const manifest = JSON.parse(await readFile(`./plugins/${plug}/manifest.json`));
+    const outPath = `./dist/${plug}/index.js`;
 
-await mkdir("./dist", { recursive: true });
+    try {
+        const bundle = await rollup({
+            input: `./plugins/${plug}/${manifest.main}`,
+            onwarn: () => {},
+            plugins,
+        });
+    
+        await bundle.write({
+            file: outPath,
+            globals(id) {
+                if (id.startsWith("@vendetta")) return id.substring(1).replace(/\//g, ".");
+                const map = {
+                    react: "window.React",
+                };
 
-try {
-    const bundle = await rollup({
-        input: `./${manifest.main}`,
-        onwarn: () => {},
-        plugins,
-    });
-
-    await bundle.write({
-        file: outPath,
-        globals(id) {
-            if (id.startsWith("@vendetta")) return id.substring(1).replace(/\//g, ".");
-            return { react: "window.React" }[id] || null;
-        },
-        format: "iife",
-        compact: true,
-        exports: "named",
-    });
-    await bundle.close();
-
-    const toHash = await readFile(outPath);
-    manifest.hash = createHash("sha256").update(toHash).digest("hex");
-    manifest.main = "index.js";
-    await writeFile("./dist/manifest.json", JSON.stringify(manifest));
-
-    console.log(`Successfully built ${manifest.name}!`);
-} catch (e) {
-    console.error("Failed to build plugin:", e);
-    process.exit(1);
+                return map[id] || null;
+            },
+            format: "iife",
+            compact: true,
+            exports: "named",
+        });
+        await bundle.close();
+    
+        const toHash = await readFile(outPath);
+        manifest.hash = createHash("sha256").update(toHash).digest("hex");
+        manifest.main = "index.js";
+        await writeFile(`./dist/${plug}/manifest.json`, JSON.stringify(manifest));
+    
+        console.log(`Successfully built ${manifest.name}!`);
+    } catch (e) {
+        console.error("Failed to build plugin...", e);
+        process.exit(1);
+    }
 }
